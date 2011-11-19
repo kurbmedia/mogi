c1292029107b23ae6aba4919a415458da3eb0288
Brent Kirby <brent@kurbmedia.com>
2011-11-19 02:48:39 -0500

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mogi::Application.config.secret_token = '70c1d99189e7b282b1e1fd53075bbedc8b81d605bbcbf404971063d95ef1b4f8207f3904f869b79be99890bd6c15bce206a2313c015dddd42ab0c95eb6d30b75'
