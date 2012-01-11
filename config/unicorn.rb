36e1a402c48570a76b83601699dfb49a26e3ff00
Brent Kirby <brent@kurbmedia.com>
2012-01-10 23:35:53 -0500

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,3 +8,16 @@ pid "/git/mogi/tmp/unicorn.pid"
 
 stderr_path "/git/mogi/log/unicorn.stderr.log"
 stdout_path "/git/mogi/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+  
+end
